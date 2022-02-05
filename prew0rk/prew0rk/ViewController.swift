51646a5aa02a07f95ddbecbe820b872cbf6931b1
nfliaha <nfliaha1@gmail.com>
2022-02-05 01:37:13 -0500

    Added prework files to repositor

---
+++ prew0rk/prew0rk/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  prew0rk
+//
+//  Created by Nadzeya Fliaha on 2/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.orange
+    }
+    
+}
+
